d875fa537e8579f502851be76115edeee51d542a
huangwanneng2 <l>
2020-11-30 22:45:43 +0800

    init

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -64,7 +64,7 @@ export default {
   metaInfo: {
     meta: [
       {
-        httpEquiv: 'Content-Security-Policy',
+        'http-equiv': 'Content-Security-Policy',
         content: 'upgrade-insecure-requests',
       },
     ],
